f38d3db748f202366382934a862b45558a928a2c
김혁진 <kimhyuckjin7856@gmail.com>
2024-09-27 21:26:11 +0900

    add new file

---
+++ 김혁진_문제.js
@@ -0,0 +1,11 @@
+
+// 1. 홀수와 짝수 판별 함수
+// 숫자를 입력받아, 해당 숫자가 홀수인지 짝수인지 출력하는 함수를 만드세요.
+
+
+function checkOddOrEven(num) {
+  // 여기에 코드를 작성하세요
+}
+
+checkOddOrEven(5); // "Odd"
+checkOddOrEven(8); // "Even"
