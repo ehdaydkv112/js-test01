0fb955db17ab581577acf719d055dfce75e37b6e
seongsuuu <robot0418@naver.com>
2024-09-27 22:18:02 +0900

    ddd

---
+++ 김성수_정답.js
@@ -0,0 +1,26 @@
+// 강수정_문제
+function type_check(a) {
+  let temp = 0;
+  for (let i = 0; i <= a; i++) {
+    return temp += i
+  }
+}
+
+// 김혁진_문제
+
+function checkOddOrEven(num) {
+  if (num % 2 === 0) {
+    console.log("Even")
+  } else console.log("Odd");
+}
+
+//김두봉_문제
+
+const fullWords = [
+  '겉은 바삭하고 속은 촉촉한',
+  '아이스 바닐라 라떼',
+  '얼어 죽어도 아이스',
+  '별결 다 줄인다',
+  '만나서 반가워 잘 부탁해'
+];
+ㅏㅏㅏ
