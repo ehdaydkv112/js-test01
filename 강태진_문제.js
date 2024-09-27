d66c4a56a9a198b792402cf4091a51f1d5569389
ehdaydkv112 <ehdaydkv112@naver.com>
2024-09-27 20:55:37 +0900

    fix

--- 강태진_문제.js
+++ 강태진_문제.js
@@ -1,4 +1,5 @@
 /*
+1번 문제
 함수 이름 type_check를 만든다.
 
 매개변수로 a를 받아서,
@@ -10,4 +11,23 @@ for문을 만들어서 a만큼 반복문을 돌려야한다.
 a가 자료형이 문자열이라면
 객체를 만들고,
 그 객체에 text라는 key에 a를 value로 담아서, return한다.
+*/
+
+/*
+2번 문제
+함수 find_largest_number 를 만듭니다.
+세 개의 숫자 x, y, z를 입력받습니다.
+if 문을 사용하여 가장 큰 숫자를 찾아 반환합니다.
+*/
+
+/*
+3번 문제
+함수 find_longest_word를 만듭니다.
+문자열 str을 입력받습니다.
+문자열 안에 있는 단어들 중 가장 긴 단어를 반환합니다.
+공백으로 단어를 나누며, 같은 길이의 단어가 여러 개일 경우 첫 번째 단어를 반환합니다.
+
+출력 예시)
+find_longest_word("add good life") => "good"
+find_longest_word("add good lifeAll") => "lifeAll"
 */
